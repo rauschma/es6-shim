67972a5f5a1ddfa373138c84add070e5bf1a638d
DomenicDenicola <domenic@domenicdenicola.com>
2011-11-07 13:44:25 -0500

    Wrapping in IIFE.

--- es6-shim.js
+++ es6-shim.js
@@ -4,62 +4,64 @@
 // TODO: http://wiki.ecmascript.org/doku.php?id=harmony:proposals - API improvements
 // TODO: http://www.2ality.com/2011/11/uncurrying-this.html
 
-//----------------- Strings
-
-//// http://wiki.ecmascript.org/doku.php?id=harmony:string.prototype.repeat
-
-if (!String.prototype.repeat) {
-    String.prototype.repeat = function(times) {
-        return new Array(times+1).join(this);
-    };
-}
-
-//// http://wiki.ecmascript.org/doku.php?id=harmony:string_extras
-
-if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function(s) {
-        return this.indexOf(s) === 0;
-    };
-}
-
-if (!String.prototype.endsWith) {
-    String.prototype.endsWith = function(s) {
-        var t = String(s);
-        return this.lastIndexOf(t) === this.length - t.length;
-    };
-}
-
-if (!String.prototype.contains) {
-    String.prototype.contains = function(s) {
-        return this.indexOf(s) !== -1;
-    };
-}
-
-if (!String.prototype.toArray) {
-    String.prototype.toArray = function() {
-        return this.split('');
-    }
-}
-
-
-//----------------- Arrays
-// http://www.2ality.com/2011/07/array-from.html
-
-if (!Array.from) {
-    Array.from = function(arrayLike) {
-        return Array.prototype.slice.call(arrayLike);
-    };
-}
-
-if (!Array.of) {
-    Array.of = function() {
-        return [].slice.call( arguments );
-    };
-}
-
-//----------------- 
-
 (function (global) {
+    "use strict";
+
+    //----------------- Strings
+
+    //// http://wiki.ecmascript.org/doku.php?id=harmony:string.prototype.repeat
+
+    if (!String.prototype.repeat) {
+        String.prototype.repeat = function(times) {
+            return new Array(times+1).join(this);
+        };
+    }
+
+    //// http://wiki.ecmascript.org/doku.php?id=harmony:string_extras
+
+    if (!String.prototype.startsWith) {
+        String.prototype.startsWith = function(s) {
+            return this.indexOf(s) === 0;
+        };
+    }
+
+    if (!String.prototype.endsWith) {
+        String.prototype.endsWith = function(s) {
+            var t = String(s);
+            return this.lastIndexOf(t) === this.length - t.length;
+        };
+    }
+
+    if (!String.prototype.contains) {
+        String.prototype.contains = function(s) {
+            return this.indexOf(s) !== -1;
+        };
+    }
+
+    if (!String.prototype.toArray) {
+        String.prototype.toArray = function() {
+            return this.split('');
+        }
+    }
+
+
+    //----------------- Arrays
+    // http://www.2ality.com/2011/07/array-from.html
+
+    if (!Array.from) {
+        Array.from = function(arrayLike) {
+            return Array.prototype.slice.call(arrayLike);
+        };
+    }
+
+    if (!Array.of) {
+        Array.of = function() {
+            return [].slice.call( arguments );
+        };
+    }
+
+    //----------------- 
+
     var abs = Math.abs,
         floor = Math.floor,
         isFinite = global.isFinite,
@@ -80,16 +82,14 @@ if (!Array.of) {
         enumerable: false,
         writable: true
     });
-})(this);
 
-
-if (!Object.getOwnPropertyDescriptors) {
-    Object.getOwnPropertyDescriptors = function (obj) {
-        var descs = {};
-        Object.getOwnPropertyNames(obj).forEach(function(propName) {
-            descs[propName] = Object.getOwnPropertyDescriptor(obj, propName);
-        });
-        return descs;
-    };
-}
-
+    if (!Object.getOwnPropertyDescriptors) {
+        Object.getOwnPropertyDescriptors = function (obj) {
+            var descs = {};
+            Object.getOwnPropertyNames(obj).forEach(function(propName) {
+                descs[propName] = Object.getOwnPropertyDescriptor(obj, propName);
+            });
+            return descs;
+        };
+    }
+}(this));
