6e9e7a239160f9b15cd492ec9e65f96676819f4b
DomenicDenicola <domenic@domenicdenicola.com>
2011-11-07 13:57:05 -0500

    Uniformizing the style in which methods are attached.
    All now use the `method` or `methods` helper, which checks to see if a property exists before defining it to be a configurable, non-enumerable, writable property.
    
    Also uniformized commenting/sectioning style and added a few more links.

--- es6-shim.js
+++ es6-shim.js
@@ -2,65 +2,61 @@
 // (sometimes with minor modifications)
 
 // TODO: http://wiki.ecmascript.org/doku.php?id=harmony:proposals - API improvements
+// TODO: http://wiki.ecmascript.org/doku.php?id=harmony:extended_object_api
 // TODO: http://www.2ality.com/2011/11/uncurrying-this.html
 
 (function (global) {
     "use strict";
 
-    //----------------- Strings
-
-    //// http://wiki.ecmascript.org/doku.php?id=harmony:string.prototype.repeat
-
-    if (!String.prototype.repeat) {
-        String.prototype.repeat = function(times) {
-            return new Array(times+1).join(this);
-        };
-    }
-
-    //// http://wiki.ecmascript.org/doku.php?id=harmony:string_extras
-
-    if (!String.prototype.startsWith) {
-        String.prototype.startsWith = function(s) {
-            return this.indexOf(s) === 0;
-        };
-    }
-
-    if (!String.prototype.endsWith) {
-        String.prototype.endsWith = function(s) {
-            var t = String(s);
-            return this.lastIndexOf(t) === this.length - t.length;
-        };
-    }
-
-    if (!String.prototype.contains) {
-        String.prototype.contains = function(s) {
-            return this.indexOf(s) !== -1;
-        };
-    }
-
-    if (!String.prototype.toArray) {
-        String.prototype.toArray = function() {
-            return this.split('');
+    function method(obj, name, method) {
+        if (!obj[methodName]) {
+            Object.defineProperty(obj, name, { configurable: true, enumerable: false, writable: true, value: method });
         }
     }
 
+    function methods(obj, map) {
+        Object.keys(map).forEach(function (name) {
+            method(obj, name, map[name]);
+        });
+    }
 
-    //----------------- Arrays
-    // http://www.2ality.com/2011/07/array-from.html
+    //----------------- String.prototype
+    //// http://wiki.ecmascript.org/doku.php?id=harmony:string.prototype.repeat
+    //// http://wiki.ecmascript.org/doku.php?id=harmony:string_extras
 
-    if (!Array.from) {
-        Array.from = function(arrayLike) {
+    methods(String.prototype, {
+        repeat: function (times) {
+            return new Array(times + 1).join(this);
+        },
+        startsWith: function (s) {
+            return this.indexOf(s) === 0;
+        },
+        endsWith: function (s) {
+            var t = String(s);
+            return this.lastIndexOf(t) === this.length - t.length;
+        },
+        contains: function (s) {
+            return this.indexOf(s) !== -1;
+        },
+        toArray: function () {
+            return this.split('');
+        }
+    });
+
+    //----------------- Array
+    //// http://www.2ality.com/2011/07/array-from.html
+    //// http://wiki.ecmascript.org/doku.php?id=strawman:array_extras
+
+    methods(Array, {
+        from: function (arrayLike) {
             return Array.prototype.slice.call(arrayLike);
-        };
-    }
+        },
+        of: function () {
+            return Array.prototype.slice.call(arguments);
+        }
+    });
 
-    if (!Array.of) {
-        Array.of = function() {
-            return [].slice.call( arguments );
-        };
-    }
-
-    //----------------- 
+    //----------------- Number
 
     var abs = Math.abs,
         floor = Math.floor,
@@ -68,28 +64,25 @@
         isNaN = global.isNaN;
         
     function sign(n) { return (n < 0) ? -1 : 1; }
- 
-    Object.defineProperty(Number, 'toInteger', {
-        value: function toInteger(value) {
-            var n = +value;
-            if (isNaN(n))
-                return +0;
-            if (n === 0 || !isFinite(n))
-                return n;
-            return sign(n) * floor(abs(n));
-        },
-        configurable: true,
-        enumerable: false,
-        writable: true
+
+    //// http://wiki.ecmascript.org/doku.php?id=harmony:number.tointeger
+    method(Number, "toInteger", function (value) {
+        var n = +value;
+        if (isNaN(n))
+            return +0;
+        if (n === 0 || !isFinite(n))
+            return n;
+        return sign(n) * floor(abs(n));
     });
 
-    if (!Object.getOwnPropertyDescriptors) {
-        Object.getOwnPropertyDescriptors = function (obj) {
-            var descs = {};
-            Object.getOwnPropertyNames(obj).forEach(function(propName) {
-                descs[propName] = Object.getOwnPropertyDescriptor(obj, propName);
-            });
-            return descs;
-        };
-    }
+    //----------------- Object
+
+    //// http://wiki.ecmascript.org/doku.php?id=strawman:extended_object_api
+    method(Object, "getOwnPropertyDescriptors", function (obj) {
+        var descs = {};
+        Object.getOwnPropertyNames(obj).forEach(function(propName) {
+            descs[propName] = Object.getOwnPropertyDescriptor(obj, propName);
+        });
+        return descs;
+    });
 }(this));
